d91c0ac843ba2b663da750eae010882d9aea7cc8
Vladyslav Rohalov <domus.lg@gmail.com>
2022-12-01 13:07:11 +0100

    add hw-07
    add solution for the task 1 and task 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryCollectionRef = document.querySelector('.gallery');
+
+function createMarkup() {
+  const galleryCollection = galleryItems
+    .map(({ original, preview, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+    })
+    .join('');
+  galleryCollectionRef.insertAdjacentHTML('beforeend', galleryCollection);
+}
+
+createMarkup();
+
+galleryCollectionRef.addEventListener('click', onOpenModal);
+
+let instance;
+
+function onOpenModal(evt) {
+  disableLink(evt);
+  window.addEventListener('keydown', onEscKeyPress);
+  const largeImg = evt.target.dataset.source;
+  instance = basicLightbox.create(`
+    <img src="${largeImg}" width="1280">
+`);
+  instance.show();
+}
+
+function disableLink(evt) {
+  evt.preventDefault();
+}
+
+function onEscKeyPress(evt) {
+  const ESC_KEY_CODE = 'Escape';
+  if (evt.code === ESC_KEY_CODE) {
+    instance.close();
+    window.removeEventListener('keydown', onEscKeyPress);
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryCollectionRef = document.querySelector('.gallery');
+
+function createMarkup() {
+  const galleryCollection = galleryItems
+    .map(({ original, preview, description }) => {
+      return `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+    })
+    .join('');
+  galleryCollectionRef.insertAdjacentHTML('beforeend', galleryCollection);
+}
+
+createMarkup();
+
+galleryCollectionRef.addEventListener('click', onOpenModal);
+
+function onOpenModal(evt) {
+  disableLink(evt);
+  let gallery = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+  });
+  gallery.on('show.simplelightbox');
+  console.log(evt.target.alt);
+}
+
+function disableLink(evt) {
+  evt.preventDefault();
+}
